9d7bdcb9dcdf49fa9c27baf89cd480c99c3c4617
Muhammad Naufal Al Kayyis <naufalkayyiss@gmail.com>
2025-10-13 23:11:48 +0700

    Update matrix and linear regeression modules

---
+++ module/__init__.py
@@ -0,0 +1,16 @@
+from .matrix_operations import (
+    add_matrices,
+    subtract_matrices,
+    multiply_matrices,
+    transpose_matrix,
+    inverse_matrix
+)
+
+from .regression import (
+    linear_regression,
+    predict
+)
+
+from .utils import (
+    read_matrix_from_file
+)
